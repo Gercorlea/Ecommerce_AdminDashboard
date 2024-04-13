e11fb74614064fbcd43db97cbbd905ed5ea91cdf
Gera Cortes <geracor15@gmail.com>
2024-04-13 04:27:42 -0600

    Clerk sign-up fix

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -20,7 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements: {
+        footer: "hidden",
+      },
+    }}>
       <html lang="en">
         <body className={inter.className}>
           <ToasterProvider/>
