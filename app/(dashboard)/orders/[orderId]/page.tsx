ca47c96166c75cb15c9f2bffd00b3b43a503254e
Gera Cortes <geracor15@gmail.com>
2024-04-13 02:14:35 -0600

    productId_GET orderId_GET Fix v3

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -2,17 +2,12 @@ import { DataTable } from "@/components/custom ui/DataTable";
 import { columns } from "@/components/orderItems/orderItemsColumns";
 
 const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
-  const res = await fetch(`/api/orders/${params.orderId}`);
+  const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`);
 
   const { orderDetails, customer } = await res.json();
 
   const { street1, street2, city, state, postalCode, country } =
     orderDetails.shippingAddress;
-
-  // const orderProducts = [];
-  // orderDetails.products.map((orderDetail: any) => {
-  //   orderProducts.push([...orderDetail, product: orderDetail.product.toString()])
-  // });
 
   return (
     <div className="flex flex-col p-10 gap-5">
